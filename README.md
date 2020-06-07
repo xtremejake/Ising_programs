eb5cec39ef25a75e69a302cb83026a82e34364e3
xtremejake <xtremejake.usa@gmail.com>
2020-06-07 11:17:11 -0400

    adding LICENSE.txt, updating README.md, improvements to setup.py

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 # Ising_programs
 
-May 23, 2020
-Doug Barrick
-
 A suite of python programs to analyze repeat-protein unfolding data with a 1D Ising model.
 
 This repository contains four folders.  The two main folders contain scripts to run 1D Ising analysis.
@@ -13,8 +10,33 @@ In addition there is a folder with some additional scripts to convert and merge 
 
 Finally, there is a folder for calculation of partial correlation coefficients from bootstrapped thermodynamic parameters.
 
-All folders contain data files on which the scripts can be run.  All programs were written to run in python 2.7 except 
-for the partial correlation program, which requires python 3.7.
+All folders contain data files on which the scripts can be run.  All programs were written to run in python 3.8.
 
 A detailed description of this suite of programs and its applications will soon be submitted to the journal Protein Sciene
 for publication.  A preprint can be obtained from Doug Barrick (barrick@jhu.edu).
+
+## License
+[License](LICENSE.txt)
+
+## Setup
+
+```
+#git clone and install the package (pypi submission to come at a later date)
+git clone https://github.com/barricklab-at-jhu/Ising_programs.git```
+
+
+# pip intall the package
+pip install .
+```
+## Quikstart
+1. To run the code via the notebooks:
+   * ```cd <homopolymer_fit OR heteropolymer_fit>```
+   * ```jupyter notebook```
+   * click on ```Ising_fitter_heteropolymer.ipynb``` OR ```Ising_fitter_homopolymer.ipynb```
+
+2. To run via the the ```.py``` files:
+## Examples
+* Check [Examples](docs/getting_started/examples.md) to view examples of systems modeled using Ising.py
+
+## Q&A
+[FAQ](FAQ.md)
--- setup.py
+++ setup.py
@@ -1,29 +1,24 @@
 import setuptools
-from os import path
-
-FILE_DIR = path.dirname(path.abspath(__file__))
-README_FILE = path.join(path.dirname(FILE_DIR), 'README.md')
-REQUIREMENTS_FILE = path.join(path.dirname(FILE_DIR), 'requirements.txt')
-
-# description
-with open(README_FILE, "r") as fh:
-    long_description = fh.read()
-
-# package requirements
-with open(REQUIREMENTS_FILE, 'r') as fh:
-    requirements = fh.read()
 
 setuptools.setup(
-    name='ising',
-    version='0.1',
-    description='For global fitting of systems represented by 1D Ising Models',
-    url='https://github.com/barricklab-at-jhu/Ising_programs',
-    author='barricklab-at-jhu, xtremejake',
-    author_email='barrick@jhu.edu, xtremejake.usa@gmail.com',
-    license='Apache-2.0',
-    #install_requires=[] # specifies dependencies of python packages in pip
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    name="ising",
+    version="0.1",
+    description="For global fitting of systems represented by 1D Ising Models",
+    url="https://github.com/barricklab-at-jhu/Ising_programs",
+    author="barricklab-at-jhu, xtremejake",
+    author_email="barrick@jhu.edu, xtremejake.usa@gmail.com",
+    license="Apache-2.0",
+    include_package_metadata=True,
+    install_requires=[
+        "black==19.10b0",
+        "jupyter==1.0.0",
+        "lmfit==1.0.1",
+        "numpy==1.18.5",
+        "pandas==1.0.4",
+        "sympy==1.6",
+    ],  # specifies dependencies of python packages in pip
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     zip_safe=True,
     classifiers=[
